d4cb379ab6a1cec64ee8746d24a8397cec3b896e
rzadp <roopert7@gmail.com>
2023-09-05 14:12:29 +0200

    Instructions

--- src/handle-command.ts
+++ src/handle-command.ts
@@ -22,15 +22,18 @@ export const handleCommand = async (opts: {
           "```\n/rfc process 0x39fbc57d047c71f553aa42824599a7686aea5c9aab4111f6b836d35d3d058162\n```\n" +
           `\n<details><summary>Instructions to find the block hash</summary>` +
           "Here is one way to find the corresponding block hash." +
-          `\n\n1. Open the referendum on Subsquare` +
+          `\n\n1. Open the referendum on Subsquare.` +
           "\n\n2. Switch to the `Timeline` tab." +
-          `\n<img width="480px" src="https://raw.githubusercontent.com/paritytech/rfc-propose/rzadp/fellowship-process-bot/src/images/timeline_tab.png" />` +
-          "\n\n3. Go to the details of the `Confirmed` event" +
+          `\n<img width="250px" src="https://raw.githubusercontent.com/paritytech/rfc-propose/rzadp/fellowship-process-bot/src/images/timeline_tab.png" />` +
+          "\n\n---" +
+          "\n\n3. Go to the details of the `Confirmed` event." +
           `\n<img width="480px" src="https://raw.githubusercontent.com/paritytech/rfc-propose/rzadp/fellowship-process-bot/src/images/confirmed_event.png" />` +
+          "\n\n---" +
           `\n\n2. Go to the details of the block containing that event.` +
-          `\n<img width="480px" src="https://raw.githubusercontent.com/paritytech/rfc-propose/rzadp/fellowship-process-bot/src/images/block_number.png" />` +
+          `\n<img width="380px" src="https://raw.githubusercontent.com/paritytech/rfc-propose/rzadp/fellowship-process-bot/src/images/block_number.png" />` +
+          "\n\n---" +
           `\n\n2. Here you can find the block hash.` +
-          `\n<img width="480px" src="https://raw.githubusercontent.com/paritytech/rfc-propose/rzadp/fellowship-process-bot/src/images/block_hash.png" />` +
+          `\n<img width="620px" src="https://raw.githubusercontent.com/paritytech/rfc-propose/rzadp/fellowship-process-bot/src/images/block_hash.png" />` +
           `\n</details>`,
       };
     }
