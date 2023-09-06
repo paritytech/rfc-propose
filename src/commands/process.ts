ba0e00e9c0e62bd1c1d384c1b28ee102f4503d08
rzadp <roopert7@gmail.com>
2023-09-06 10:58:00 +0200

    Instructions

--- src/commands/process.ts
+++ src/commands/process.ts
@@ -4,7 +4,37 @@ import { findReferendum } from "../find-referendum";
 import { RequestResult, RequestState } from "../types";
 import { parseRFC } from "./common/parse-RFC";
 
-export const handleProcessCommand = async (requestState: RequestState, blockHash: string): Promise<RequestResult> => {
+const blockHashInstructions =
+  `<details><summary>Instructions to find the block hash</summary>` +
+  "Here is one way to find the corresponding block hash." +
+  `\n\n1. Open the referendum on Subsquare.` +
+  "\n\n2. Switch to the `Timeline` tab." +
+  `\n<img width="250px" src="https://raw.githubusercontent.com/paritytech/rfc-propose/main/src/images/timeline_tab.png" />` +
+  "\n\n---" +
+  "\n\n3. Go to the details of the `Confirmed` event." +
+  `\n<img width="480px" src="https://raw.githubusercontent.com/paritytech/rfc-propose/main/src/images/confirmed_event.png" />` +
+  "\n\n---" +
+  `\n\n2. Go to the details of the block containing that event.` +
+  `\n<img width="380px" src="https://raw.githubusercontent.com/paritytech/rfc-propose/main/src/images/block_number.png" />` +
+  "\n\n---" +
+  `\n\n2. Here you can find the block hash.` +
+  `\n<img width="620px" src="https://raw.githubusercontent.com/paritytech/rfc-propose/main/src/images/block_hash.png" />` +
+  `\n</details>`;
+
+export const handleProcessCommand = async (
+  requestState: RequestState,
+  blockHash: string | undefined,
+): Promise<RequestResult> => {
+  if (!blockHash && !blockHash?.startsWith("0x")) {
+    return {
+      success: false,
+      errorMessage:
+        "Please provider a block hash where the referendum confirmation event is to be found.\n" +
+        "For example:\n\n" +
+        "```\n/rfc process 0x39fbc57d047c71f553aa42824599a7686aea5c9aab4111f6b836d35d3d058162\n```\n\n" +
+        blockHashInstructions,
+    };
+  }
   const parseRFCResult = await parseRFC(requestState);
   if ("success" in parseRFCResult) {
     return parseRFCResult;
@@ -14,7 +44,7 @@ export const handleProcessCommand = async (requestState: RequestState, blockHash
   if (!referendum) {
     return {
       success: false,
-      errorMessage: `Unable to find the referendum confirm event in the given block.`,
+      errorMessage: `Unable to find the referendum confirm event in the given block.\n\n` + blockHashInstructions,
     };
   }
   if ("approved" in referendum && referendum.approved) {
--- src/handle-command.ts
+++ src/handle-command.ts
@@ -12,32 +12,8 @@ export const handleCommand = async (opts: {
     return await handleProposeCommand(requestState);
   }
   if (command?.toLowerCase() === "process") {
-    const arg = args[0];
-    if (!arg && !arg?.startsWith("0x")) {
-      return {
-        success: false,
-        errorMessage:
-          "Please provider a block hash where the referendum confirmation event is to be found.\n" +
-          "For example:\n\n" +
-          "```\n/rfc process 0x39fbc57d047c71f553aa42824599a7686aea5c9aab4111f6b836d35d3d058162\n```\n" +
-          `\n<details><summary>Instructions to find the block hash</summary>` +
-          "Here is one way to find the corresponding block hash." +
-          `\n\n1. Open the referendum on Subsquare.` +
-          "\n\n2. Switch to the `Timeline` tab." +
-          `\n<img width="250px" src="https://raw.githubusercontent.com/paritytech/rfc-propose/main/src/images/timeline_tab.png" />` +
-          "\n\n---" +
-          "\n\n3. Go to the details of the `Confirmed` event." +
-          `\n<img width="480px" src="https://raw.githubusercontent.com/paritytech/rfc-propose/main/src/images/confirmed_event.png" />` +
-          "\n\n---" +
-          `\n\n2. Go to the details of the block containing that event.` +
-          `\n<img width="380px" src="https://raw.githubusercontent.com/paritytech/rfc-propose/main/src/images/block_number.png" />` +
-          "\n\n---" +
-          `\n\n2. Here you can find the block hash.` +
-          `\n<img width="620px" src="https://raw.githubusercontent.com/paritytech/rfc-propose/main/src/images/block_hash.png" />` +
-          `\n</details>`,
-      };
-    }
-    return await handleProcessCommand(requestState, arg);
+    const blockHash = args[0];
+    return await handleProcessCommand(requestState, blockHash);
   }
   return {
     success: false,
