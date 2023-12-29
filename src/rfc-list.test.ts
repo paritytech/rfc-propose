2b38241112f0b53319cbd041659bc3973be5ea4a
Javier Bullrich <javier@bullrich.dev>
2023-12-29 16:27:41 -0300

    imrpved logic to list all rfc and filter valid ones

--- src/rfc-list.test.ts
+++ src/rfc-list.test.ts
@@ -1,28 +1,51 @@
 import { ApiPromise, WsProvider } from "@polkadot/api";
 
 describe("RFC Listing test", () => {
+  let wsProvider: WsProvider;
   test.only("Should get proposals", async () => {
-    const wsProvider = new WsProvider("wss://polkadot-collectives-rpc.polkadot.io");
-    try {
-      const api = await ApiPromise.create({ provider: wsProvider });
-      // We fetch all the members
-      const query = (await api.query.fellowshipReferenda.referendumCount.size()).toPrimitive();
-      console.log("referendumCount", query);
+    wsProvider = new WsProvider("wss://polkadot-collectives-rpc.polkadot.io");
+    const api = await ApiPromise.create({ provider: wsProvider });
+    // We fetch all the members
+    const query = (await api.query.fellowshipReferenda.referendumCount()).toPrimitive();
+    console.log("referendumCount", query);
 
-      if (typeof query !== "number") {
-        throw new Error(`Query result is not a number: ${query}`);
-      }
-      for (const index of Array.from(Array(query).keys())) {
-        console.log("Fetching element N", index);
-
-        const refQuery = (await api.query.fellowshipReferenda.referendumInfoFor(index)).toJSON();
-        console.log("Reference query", refQuery);
-      }
-    } catch (e) {
-      console.error("Operation failed");
-      throw e;
-    } finally {
-      await wsProvider.disconnect();
+    if (typeof query !== "number") {
+      throw new Error(`Query result is not a number: ${query}`);
     }
+    const ongoing: OnGoing[] = [];
+    for (const index of Array.from(Array(query).keys())) {
+      console.log("Fetching element %s/%s", index, query);
+
+      const refQuery = (await api.query.fellowshipReferenda.referendumInfoFor(index)).toJSON() as { ongoing?: OnGoing };
+      console.log("Reference query", refQuery);
+      if (refQuery.ongoing) {
+        ongoing.push(refQuery.ongoing);
+      }
+    }
+
+    console.log(`Found ${ongoing.length} ongoing requests`, ongoing);
+  }, 20_000);
+
+  afterEach(async () => {
+    await wsProvider.disconnect();
   });
 });
+
+interface OnGoing {
+  track: number;
+  origin: { fellowshipOrigins: string };
+  proposal: { lookup: any };
+  enactment: { after: number };
+  submitted: number;
+  submissionDeposit: {
+    who: string;
+    amount: number;
+  };
+  decisionDeposit: {
+    who: string;
+    amount: number;
+  };
+  deciding: { since: number; confirming: null };
+  tally: Record<string, number>;
+  inQueue: boolean;
+}
