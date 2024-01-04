7718400fe35451df0c7a913dfb0e23b3b4851c4d
Javier Bullrich <javier@bullrich.dev>
2024-01-04 10:32:04 -0300

    added ability to fetch the remark

--- src/rfc-list.test.ts
+++ src/rfc-list.test.ts
@@ -1,8 +1,32 @@
 import { ApiPromise, WsProvider } from "@polkadot/api";
+import fetch from "node-fetch";
+
+const getReferendaData = async (track: number): Promise<ReferendaData> => {
+  const url = `https://collectives.subsquare.io/api/fellowship/referenda/${track}.json`;
+  const call = await fetch(url);
+  const data = (await call.json()) as ReferendaData;
+  console.log("Parsed data is", data);
+  return data;
+};
+
+const hexToString = (hex: string) => {
+  let str = "";
+  for (let i = 0; i < hex.length; i += 2) {
+    const hexValue = hex.substr(i, 2);
+    const decimalValue = parseInt(hexValue, 16);
+    str += String.fromCharCode(decimalValue);
+  }
+  return str;
+};
 
 describe("RFC Listing test", () => {
   let wsProvider: WsProvider;
   test.only("Should get proposals", async () => {
+    const ref54 = await getReferendaData(54);
+    if (ref54.onchainData?.inlineCall?.call?.args) {
+      const [callData] = ref54.onchainData?.inlineCall?.call.args;
+      console.log("Hex value is %s. Converted value is %s", callData.value, hexToString(callData.value));
+    }
     wsProvider = new WsProvider("wss://polkadot-collectives-rpc.polkadot.io");
     const api = await ApiPromise.create({ provider: wsProvider });
     // We fetch all the members
@@ -20,6 +44,7 @@ describe("RFC Listing test", () => {
       console.log("Reference query", refQuery);
       if (refQuery.ongoing) {
         ongoing.push(refQuery.ongoing);
+        const referendaData = await getReferendaData(refQuery.ongoing.track);
       }
     }
 
@@ -48,4 +73,39 @@ interface OnGoing {
   deciding: { since: number; confirming: null };
   tally: Record<string, number>;
   inQueue: boolean;
+}
+
+interface ReferendaData {
+  _id: string;
+  referendumIndex: number;
+  proposer: string;
+  title: string;
+  content: string;
+  contentType: string | "markdown";
+  track: number;
+  createdAt: Date;
+  updatedAt: Date;
+  edited: boolean;
+  onchainData?: {
+    _id: string;
+    referendumIndex: number;
+    track: number;
+    state: {
+      name: string;
+    };
+    inlineCall?: {
+      hex: string;
+      hash: string;
+      call?: {
+        callIndex: string;
+        section: string;
+        method: "remark" | string;
+        args: {
+          name: "remark" | string;
+          type: "Bytes" | string;
+          value: string;
+        }[];
+      };
+    };
+  };
 }
