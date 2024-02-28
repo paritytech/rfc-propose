51be37c3fbbb561058e7faef19c4ac5a2577d00f
Javier Bullrich <javier@bullrich.dev>
2024-02-28 16:57:27 +0100

    fixed test not working

--- src/cron.test.ts
+++ src/cron.test.ts
@@ -2,7 +2,7 @@ import { getAllRFCRemarks } from "./cron";
 
 describe("RFC Listing test", () => {
   test("Should not return any remark with future date", async () => {
-    const remarks = await getAllRFCRemarks(new Date());
-    expect(remarks).toHaveLength(0);
+    const { ongoing } = await getAllRFCRemarks(new Date());
+    expect(ongoing).toHaveLength(0);
   }, 60_000);
 });
