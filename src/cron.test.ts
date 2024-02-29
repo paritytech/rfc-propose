aee354ff0355b7dbd9511c7f9c092d705fa4d55f
Javier Bullrich <javier@bullrich.dev>
2024-02-29 11:51:59 +0100

    improved tests
    Now it also fetches completed RFCs from an older date

--- src/cron.test.ts
+++ src/cron.test.ts
@@ -2,8 +2,15 @@ import { getAllRFCRemarks } from "./cron";
 
 describe("RFC Listing test", () => {
   test("Should not return any remark with future date", async () => {
-    const { ongoing } = await getAllRFCRemarks(new Date());
+    const { ongoing, completed } = await getAllRFCRemarks(new Date());
     expect(ongoing).toHaveLength(0);
-  }, 60_000);
+    expect(completed).toHaveLength(0);
+  }, 90_000);
+
+  test("Should return completed projects in the past", async () => {
+    // It feels wrong to call 2000 an 'old date'
+    const oldDate = new Date("01/01/2000");
+    const { completed } = await getAllRFCRemarks(oldDate);
+    expect(completed.length).toBeGreaterThan(0);
   }, 90_000);
 });
