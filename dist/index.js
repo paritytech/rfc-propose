93229fc8f9b19c4a12265bef3c36e4e97d1ebc7e
Javier Bullrich <javier@bullrich.dev>
2024-03-06 12:00:40 +0100

    rebuild dist

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e467da154e7c7e5a4492fbd3b29ec3ec5c5133a4d6817d08b6f6e1087408d1ea
-size 4556681
+oid sha256:281f27350a6a259f4ad255965895cb4c940e22b1304f303dcc59b48f4e4c7fd8
+size 4556601
